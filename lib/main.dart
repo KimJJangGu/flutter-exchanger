3cbafc0b637ee7d48593ac6f9f4d30bd959034b6
Kim DoHyeon <dohyun51731@gmail.com>
2024-01-08 19:12:24 +0900

    feat: ListView 데이터 연결
    김도현 커밋 5

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       routerConfig: routes,
       title: 'Flutter Demo',
       theme: ThemeData(
--- lib/presenter/exchange_screen.dart
+++ lib/presenter/exchange_screen.dart
@@ -1,4 +1,8 @@
+import 'package:exchanger/domain/model/exchange.dart';
+import 'package:exchanger/presenter/exchange_state.dart';
+import 'package:exchanger/presenter/exchange_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ExchangeScreen extends StatefulWidget {
   const ExchangeScreen({super.key});
@@ -10,6 +14,20 @@ class ExchangeScreen extends StatefulWidget {
 class _ExchangeScreenState extends State<ExchangeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final ExchangeViewModel exchangeViewModel = context.watch();
+    final ExchangeState exchangeState = exchangeViewModel.state;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('오늘의 환율'),
+      ),
+      body: ListView.builder(
+        itemCount: exchangeState.exchangeList.length,
+        itemBuilder: (context, index) {
+          final Exchange exchange = exchangeState.exchangeList[index];
+          return Text('$exchange');
+        },
+      ),
+    );
   }
 }
